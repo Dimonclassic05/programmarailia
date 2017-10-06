c86358855925267d8c62772364832aa1e9e57725
Dimonclassic05 <32394657+Dimonclassic05@users.noreply.github.com>
2017-10-06 17:47:26 +0300

    21

--- 2-6sum/main.cpp
+++ 2-6sum/main.cpp
@@ -6,11 +6,12 @@ int main()
 {
 int array[3][3];
 int sum = 0 ;
+std::cout << " enter nine numbers " ;
 for ( int j = 0; j < 3; ++j)
 {
     for ( int i = 0; i < 3; ++i)
     {
-        std::cout << " enter nine numbers " ;
+
         std::cin >> array[i][j];
         if ((i + j) % 2 == 0)
         {
