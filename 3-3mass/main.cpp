9eb844ff31417b0011897d2fe7cda3edfa8ae6d3
Dimonclassic05 <32394657+Dimonclassic05@users.noreply.github.com>
2017-10-12 16:16:16 +0300

    86894

--- 3-3mass/main.cpp
+++ 3-3mass/main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 #include <cstdlib>
 
-void puzirek(int* a,int b);
+void puzirek(int* array,int size);
 
 int main()
 {
-    int array[4] = {1,2,3,4};
+    std::cout << " enter 4 numbers " ;
+    int array[4];
+    int i;
+    for (i = 0; i < 4; i++)
+    {
+        std::cin >> array[i];
+    }
     puzirek(array,4);
+
+    for (i = 0; i < 4; i++)
+    {
+        std::cout << array[i];
+    }
+
     system("pause");
     return 0;
 }
 
-void puzirek(int* a,int b)
+void puzirek(int* array,int size)
 {
     int c;
     int j;
     int i;
-    for (j = 0; j < 3; j++) // сортировка по возрастанию
+    for (j = 0; j < size - 1; j++) // сортировка по возрастанию
     {
-        for (i = 0; i < 4; i++)
+        for (i = 0; i < size; i++)
          if (array[i]>array[i+1])
          {
             c = array[i+1];
